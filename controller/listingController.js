c10beec573f792999c0f57cb7c77a6c01a8c923e
Harishkumar79 <harishsen7976@gmail.com>
2026-01-11 20:17:26 +0530

    implement error handler

--- controller/listingController.js
+++ controller/listingController.js
@@ -1,48 +1,53 @@
 const express = require("express");
 const Listing = require("../model/listing");
+const wrapAsync = require("../utils/wrapAsync.js");
+const ExpressError = require("../utils/expressError.js");
 
-const listAllPost = async (req,res) =>{
+const listAllPost = wrapAsync(async (req,res) =>{
     const allPost = await Listing.find({});
     res.render("listingViews/listing.ejs" , {allPost});
-}
+})
 
-const listPost = async (req,res) =>{
+const listPost = wrapAsync(async (req,res) =>{
     let {id} = req.params;
     let post = await Listing.findById(id);
     res.render("listingViews/showPost.ejs",{post});
-}
+})
 
 const listNewPost = (req , res) =>{
     res.render("listingViews/createPost.ejs");
 }
 
-const listCreatePost = async (req, res) => {
+const listCreatePost = wrapAsync(async (req, res) => {
+    console.log('req.body.postInfo', req.body);
+    if(!req.body){
+        throw new ExpressError(400 , "Send valid data for listing!");
+    }
     let newListing = new Listing(req.body.postInfo);
     await newListing.save();
     res.redirect("/listing");
-}
+});
 
-const listEdit = async (req,res) =>{
+const listEdit = wrapAsync(async (req,res) =>{
     const {id} = req.params;
     let post = await Listing.findById(id);
     res.render("listingViews/editPost.ejs" , {post});
-}
+})
 
-const listUpdatePost = async (req ,res) => {
+const listUpdatePost = wrapAsync(async (req ,res) => {
+    if(!req.body){
+        throw new ExpressError(400 , "Send valid data to update!");
+    }
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.postInfo});
     res.redirect(`/listing/${id}`);
-}
+})
 
-const listDelete = async (req , res) => {
+const listDelete = wrapAsync(async (req , res) => {
     let {id} = req.params;
     const deletedListing = await Listing.findByIdAndDelete(id);
-
-    console.log("deleted" , deletedListing);
-
     res.redirect("/listing");
-
-}
+})
 
 
 module.exports = {listAllPost , listPost , listNewPost , listCreatePost , listEdit , listUpdatePost, listDelete};
--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const path = require("path");
 const mongodb = require("./config/db");
 const listingRoute = require("./routes/listRoute");
-var methodOverride = require('method-override');
+const methodOverride = require('method-override');
+const ExpressError = require("./utils/expressError");
 
 const engine = require("ejs-mate");
 
@@ -28,3 +29,12 @@ app.set("view engine" , "ejs");
 app.set("views" , path.join(__dirname , "/views"));
 
 app.use("/" , listingRoute);
+
+app.use((req , res , next)=>{
+    next(new ExpressError( 404 ,"Page not found!"));
+})
+
+app.use((err, req , res, next)=>{
+    const {statusCode = 500 , message = "something went wrong!" } = err;
+    res.status(statusCode).send(message);
+})
